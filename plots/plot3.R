30c9effd1dae38ce60666e97fea475d1c3ec088d
shivasaimarapakala <47031275+shivasaimarapakala@users.noreply.github.com>
2019-04-04 15:29:13 +0530

    Update plot3.R

--- plots/plot3.R
+++ plots/plot3.R
@@ -12,7 +12,7 @@ powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S"
 # Filter Dates for 2007-02-01 and 2007-02-02
 powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
-#creating the plot in png in the working directory
+#create the plot in png in the working directory
 png("plot3.png", width=480, height=480)
 
 # Plot 3
